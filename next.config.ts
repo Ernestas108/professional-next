5815a6d02c810949bda2d2c1934df1fa604ad8a7
Ernestas108 <ernestasnc@gmail.com>
2025-08-31 19:48:14 +0300

    chore: GH Pages basePath/assetPrefix for professional-next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
+const repo = "professional-next";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",                 // statinis export į ./out
+    images: { unoptimized: true },    // kad <Image> veiktų be Image Optimization
+    basePath: isProd ? `/${repo}` : undefined,
+    assetPrefix: isProd ? `/${repo}/` : undefined,
 };
 
 export default nextConfig;
